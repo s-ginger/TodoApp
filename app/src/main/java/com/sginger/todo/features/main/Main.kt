2771a0c4c82a3f281f28be65fe8c455b9e773189
s-ginger <jum80frol@gmail.com>
2026-07-31 17:10:09 +0600

    feat: Main.kt update

--- app/src/main/java/com/sginger/todo/features/main/Main.kt
+++ app/src/main/java/com/sginger/todo/features/main/Main.kt
@@ -1,13 +1,19 @@
 package com.sginger.todo.features.main
 
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
@@ -15,23 +21,49 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import java.util.Calendar
+import java.util.Locale
 
+
+fun getNow(): String {
+    val now = Calendar.getInstance()
+
+    val year = now.get(Calendar.YEAR)
+    val month = now.get(Calendar.MONTH) + 1
+    val day = now.get(Calendar.DAY_OF_MONTH)
+
+    val hour = now.get(Calendar.HOUR_OF_DAY)
+    val minute = now.get(Calendar.MINUTE)
+    val second = now.get(Calendar.SECOND)
+
+    return String.format(
+        Locale.getDefault(),
+        "%02d.%02d.%04d %02d:%02d:%02d",
+        day, month, year,
+        hour, minute, second
+    )
+}
 
 @Composable
 fun Main(navController: NavController) {
     var text by remember { mutableStateOf("") }
 
-    BasicTextField(
-        value = text,
-        onValueChange = { text = it },
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        textStyle = TextStyle(
-            fontSize = 16.sp
-        ),
-        cursorBrush = SolidColor(Color.Black)
-    )
+    Column(
+        modifier = Modifier.fillMaxWidth().fillMaxHeight()
+    ) {
+        Text(getNow(), modifier = Modifier.align(Alignment.CenterHorizontally))
+        BasicTextField(
+            value = text,
+            onValueChange = { text = it },
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            textStyle = TextStyle(
+                fontSize = 16.sp
+            ),
+            cursorBrush = SolidColor(Color.Black)
+        )
+    }
 }
 
 
--- app/src/main/java/com/sginger/todo/ui/MainActivity.kt
+++ app/src/main/java/com/sginger/todo/ui/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
 import com.sginger.todo.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
